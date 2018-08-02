00e809b500e89aa64e0dc8499efda1081afad676
Gekk0r <morabitodaniele@gmail.com>
2018-08-02 12:30:19 +0200

    update

--- nvm/extractionData.py
+++ nvm/extractionData.py
@@ -6,7 +6,7 @@ from scipy.spatial.distance import pdist
 
 class DataNVM(object):
 
-    def __init__(self, name, path, outpath):
+    def __init__(self, name, path):
 
         self.cameraList = []
         self.data = dict()
@@ -18,7 +18,6 @@ class DataNVM(object):
 
         self.name = name
         self.path = path
-        self.outpath = outpath
 
         file = open(self.path, 'r')
         lines = file.readlines()
@@ -26,12 +25,6 @@ class DataNVM(object):
         self.num_img = int(lines[2])
         self.index_tie = int(self.num_img + 4)
         self.num_tie = int(lines[self.index_tie])
-
-        out = open(self.outpath + "tiepoints.json", "w")
-        json_data = {"num_tie": [self.num_tie]}
-        data = json.dumps(json_data)
-        out.write(data)
-
         self.extract(lines)
 
     def dotproduct(self, v1, v2):
@@ -48,17 +41,13 @@ class DataNVM(object):
             return math.acos(elem)
 
     def extract(self, lines):
-        out = open(self.outpath + "chart_points_aggregation_tsv.tsv", "w")
+        out = open("../exampleData/chart_points_aggregation_tsv.tsv", "w")
         out.write("point_number" + "\t" + "number_images" + "\n")
-        out_2 = open(self.outpath + "pcl_redundancy.txt", "w")
 
-        for index in range(3, self.index_tie - 1):
+        for index in range(3, self.index_tie-1):
             cameraTMP = lines[index].strip().split(" ")
-            camera = {"name": cameraTMP[0].split("/")[-1], "focal_lenght": cameraTMP[1],
-                      "quaternion": [float(cameraTMP[2]), float(cameraTMP[3]), float(cameraTMP[4]),
-                                     float(cameraTMP[5])],
-                      "cameraCenter": [float(cameraTMP[6]), float(cameraTMP[7]), float(cameraTMP[8])],
-                      "radial": cameraTMP[9]}
+            camera = {"name": cameraTMP[0].split("/")[-1], "focal_lenght": cameraTMP[1], "quaternion": [cameraTMP[2], cameraTMP[3], cameraTMP[4], cameraTMP[5]],
+                      "cameraCenter": [cameraTMP[6], cameraTMP[7], cameraTMP[8]], "radial": [cameraTMP[9], cameraTMP[10], cameraTMP[11]]}
             self.cameraList.append(camera)
 
         for index in range(self.index_tie + 1, len(lines) - 1):
@@ -68,7 +57,7 @@ class DataNVM(object):
                                                                "rgb": (line[3], line[4], line[5]), "num_imgs": line[6],
                                                                "list_images": []}
 
-            out_2.write(line[0] + " " + line[1] + " " + line[2] + " " + line[6] + "\n")
+            self.interAngle(self.pointsAllData[str(index - self.index_tie)]["id"], self.pointsAllData[str(index - self.index_tie)]["point3D"], self.cameraList)
 
             self.points[str(index - self.index_tie)] = [int(line[6])]
             out.write(str(index - self.index_tie) + "\t" + line[6] + "\n")
@@ -91,37 +80,25 @@ class DataNVM(object):
                     name = self.extractName(int(line[tie]), lines)
                     self.dataAll[line[tie]] = {"name": name, "id": [line[tie + 1]],
                                                "point2D": [(line[tie + 2], line[tie + 3])]}
-                self.interAngle(self.pointsAllData[str(index - self.index_tie)]["id"],
-                                self.pointsAllData[str(index - self.index_tie)]["point3D"],
-                                self.pointsAllData[str(index - self.index_tie)]["list_images"])
 
         data = json.dumps(self.points)
-        out = open(self.outpath + "chart_points_aggregation.json", "w")
+        out = open("../exampleData/chart_points_aggregation.json", "w")
         out.write(data)
         data = json.dumps(self.data)
-        out = open(self.outpath + "chart_img_aggregation.json", "w")
+        out = open("../exampleData/chart_img_aggregation.json", "w")
         out.write(data)
         data = json.dumps(self.dataAll)
-        out = open(self.outpath + "chart_img.json", "w")
+        out = open("../exampleData/chart_img.json", "w")
         out.write(data)
         data = json.dumps(self.pointsAllData)
-        out = open(self.outpath + "chart_points.json", "w")
+        out = open("../exampleData/chart_points.json", "w")
         out.write(data)
         data = json.dumps(self.points_img)
-        out = open(self.outpath + "points_img.json", "w")
+        out = open("../exampleData/points_img.json", "w")
         out.write(data)
         data = json.dumps(self.pointAngle)
-        out = open(self.outpath + "chart_points_angle.json", "w")
+        out = open("../exampleData/chart_points_angle.json", "w")
         out.write(data)
-
-        tmpCamList = dict()
-        for camera in self.cameraList:
-            tmpCamList[camera["name"]] = [camera["quaternion"], camera["cameraCenter"]];
-        data = json.dumps(tmpCamList)
-        out = open(self.outpath + "camere.json", "w")
-        out.write(data)
-
-        return True
 
     def extractName(self, id, lines):
         name = lines[3 + id].strip().split(" ")[0]
@@ -133,32 +110,20 @@ class DataNVM(object):
 
         angle = []
         for cam in camList:
-            vx = float(pt[0]) - float(self.cameraList[int(cam)]['cameraCenter'][0])
-            vy = float(pt[1]) - float(self.cameraList[int(cam)]['cameraCenter'][1])
-            vz = float(pt[2]) - float(self.cameraList[int(cam)]['cameraCenter'][2])
+            vx = float(pt[0]) - float(cam['cameraCenter'][0])
+            vy = float(pt[1]) - float(cam['cameraCenter'][1])
+            vz = float(pt[2]) - float(cam['cameraCenter'][2])
 
             v = [vx, vy, vz]
-
-            try:
-                v = np.asarray([float(i) / sum(v) for i in v])
-                vectors.append(v)
-            except:
-                pass
-
+            v = np.asarray([float(i) / sum(v) for i in v])
+            vectors.append(v)
         for indexA in range(len(vectors)):
             for indexB in range(indexA, len(vectors)):
-                if indexA is not indexB:
-                    angle.append(pdist(np.asarray([vectors[indexA], vectors[indexB]]), self.angle)[0])
+                angle.append(pdist(np.asarray([vectors[indexA], vectors[indexB]]), self.angle)[0])
 
-        if len(angle) > 0:
-            if "%.2f" % max(angle) in self.pointAngle.keys():
-                self.pointAngle["%.2f" % max(angle)][0] += 1
-            else:
-                self.pointAngle["%.2f" % max(angle)] = [1]
+        if "%.2f" % max(angle) in self.pointAngle.keys():
+            self.pointAngle["%.2f" % max(angle)][0] += 1
+        else:
+            self.pointAngle["%.2f" % max(angle)] = [1]
 
-# DataNVM("pippo", "/media/daniele/Data/TERNA/sparse/0/file_nvm.nvm",
-#         "/media/daniele/Data/TERNA/sparse/0/")
-# DataNVM("pippo", "../../static/users/daniele/geocart_fronte/nvm/sparse.nvm", "../../static/users/daniele/geocart_fronte/statistics/")
-
-
-# pippo = DataNVM("pippo", "../exampleData/file_nvm.nvm")
+pippo = DataNVM("pippo", "../exampleData/file_nvm.nvm")
